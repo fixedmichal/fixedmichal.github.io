866c5ec33fa03cbe261cf7e3463bd2ca9b2d39df
Michal Stuleblak <michal.stuleblak@gigaset.com>
2021-12-01 11:31:55 +0100

    start

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const aboutMeSection = document.getElementsByClassName("about-me")[0];
+console.log(aboutMeSection)
+aboutMeSection.addEventListener("DOMContentLoaded", function() {
+    aboutMeSection.classList.add("anima");
+})
+
+console.log("after")
+
+console.log(aboutMeSection)
+
