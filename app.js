ed2e812ec7504f0c2dcde9aaf1c86b98c3548335
Michal Stuleblak <michal.stuleblak@gigaset.com>
2021-12-14 22:30:16 +0100

    upd

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
-
-const navigationList = document.querySelector(".navbar__navigation-links-list")
+const navbar = document.querySelector(".navbar");
+const navigationList = document.querySelector(".navbar__navigation-links-list");
 const hamburger = document.querySelector(".hamburger");
 const navigationListItemsArray = [...document.querySelectorAll(".navbar__navigation-links-list__item")];
+const closeNavbarButton = document.querySelector(".close-navbar-button");
+
+hamburger.addEventListener("click", () => {
+    navbar.classList.toggle("invert-color");
+    navigationList.classList.toggle("nav-sliding");
+    hamburger.classList.toggle("hamburger--disappearing");
+    setTimeout( () => {
+        closeNavbarButton.classList.toggle("first-item");
+        closeNavbarButton.classList.toggle("display-zero-opacity");
+    }, 600);
+    
+    setTimeout( () => {
+        closeNavbarButton.classList.toggle("full-opacity");
+
+    }, 500);
 
 
-hamburger.addEventListener("click", ()=> {
-    navigationList.classList.toggle("nav-sliding")
 })
+
+closeNavbarButton.addEventListener("click", () => {
+    navbar.classList.toggle("invert-color");
+    navigationList.classList.toggle("nav-sliding");
+    closeNavbarButton.classList.toggle("display-zero-opacity");
+    hamburger.classList.toggle("hamburger--disappearing");
+    hamburger.classList.toggle("display");
+    closeNavbarButton.classList.toggle("first-item");
+    closeNavbarButton.classList.toggle("full-opacity");
+
+
+})
+
 
 //  CLICKING ANY OF NAVIGATION ELEMENT WILL MAKE NAVIGATION TO SLIDE AWAY
 navigationListItemsArray.forEach( value => {
     value.addEventListener("click", ()=> {
         navigationList.classList.toggle("nav-sliding");
+        navbar.classList.toggle("invert-color");
+        closeNavbarButton.classList.toggle("display-zero-opacity");
+        hamburger.classList.toggle("hamburger--disappearing");
+        hamburger.classList.toggle("display");
+        closeNavbarButton.classList.toggle("first-item");
+        closeNavbarButton.classList.toggle("full-opacity");
+
     })
 });
